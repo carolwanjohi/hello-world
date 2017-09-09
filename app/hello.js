c05d2621018247bd7381363c75d037600b3caab4
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-09 14:53:11 +0300

    More TypeScript More TypeScript hello world

---
+++ app/hello.js
@@ -0,0 +1,6 @@
+var greeting = "Hi TypeScript";
+console.log(greeting);
+var favoriteGuitarist = "Jimi Hendrix";
+console.log(favoriteGuitarist);
+var favoriteGuitarists = ["Jimi Hendrix", "The Cure", "ACDC", "Guns n' Roses"];
+console.log(favoriteGuitarists);
---
+++ app/hello.ts
@@ -0,0 +1,8 @@
+var greeting : string = "Hi TypeScript";
+console.log(greeting);
+
+var favoriteGuitarist : string = "Jimi Hendrix";
+console.log(favoriteGuitarist);
+
+var favoriteGuitarists : string[] = ["Jimi Hendrix", "The Cure", "ACDC", "Guns n' Roses"];
+console.log(favoriteGuitarists);
